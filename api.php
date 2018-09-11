5efb1e82bdbb7fce6360449d766fb81b4e6f1c5f
root <robbiebinnie@gmail.com>
2018-09-11 13:50:08 +0000

    date time part added

---
+++ api.php
@@ -0,0 +1,28 @@
+<?php
+header("Content-Type:application/json");
+
+if(!empty($_GET['info']))
+{
+	$info=$_GET['info'];
+
+	if($info == "time"){
+		$datetime=date("Y-m-d H:i:s");
+		response(200,"valid request",$datetime);
+	}
+}
+else
+{
+	response(400,"Invalid Request",NULL);
+}
+
+function response($status,$status_message,$data)
+{
+	header("HTTP/1.1 ".$status);
+
+	$response['status']=$status;
+	$response['status_message']=$status_message;
+	$response['data']=$data;
+
+	$json_response = json_encode($response);
+	echo $json_response;
+}
